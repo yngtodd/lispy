e29b05a49a6f85b12ffc7059ea1268b576776fcc
yngtodd <young.todd.mk@gmail.com>
2020-12-09 23:14:23 -0500

    Add tokenizer

--- lispy/lispy.py
+++ lispy/lispy.py
@@ -1,1 +1,22 @@
-"""Main module."""
+from typing import List
+
+
+def tokenize(chars: str) -> List[str]:
+    r"""Convert characters to tokens
+
+    Args:
+        chars: program as a string
+
+    Returns:
+        tokenized strings by whitespace
+
+    Examples::
+
+        >>> tokenize("(begin (define x 10))")
+        ['(', 'begin', '(', 'define', 'x', '10', ')', ')']
+    """
+    # Add whitespace around braces to split them 
+    # from the rest of the tokens
+    chars = chars.replace("(", " ( ")
+    chars = chars.replace(")", " ) ")
+    return chars.split()
